b71df5bd5fcd0d0df4421bebd783a6a86a13841b
Abhi11433 <abhimaurya1188@gmail.com>
2026-09-14 13:52:54 +0530

    Add a new feature.

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 class test{
     public static void main(String[] args){
         System.out.println("This is my first java code for github");
+                System.out.println("Currently I am add a new Feature.");
     }
 }
